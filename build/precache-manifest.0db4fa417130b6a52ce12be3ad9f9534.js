af29048df778d1aa12117ff3dcf0ea27f944587c
Aron Barcsa-Szabo <abarcsa@gmail.com>
2020-05-21 01:33:32 +0200

    changed .gitignore

---
+++ build/precache-manifest.0db4fa417130b6a52ce12be3ad9f9534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa2f3b027747676f91e29633f9f715e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b624b6b14bc45089f3e8",
+    "url": "./static/css/main.1dbfee05.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b624b6b14bc45089f3e8",
+    "url": "./static/js/main.2b52ebe7.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
